8625d5e7c8eeabfab08c67ed6901256bfac821b2
FinnT730 <finnt730>
2017-11-30 17:41:52 +0100

    Added color's and numbers in the rectangle's

--- src/app/Display.java
+++ src/app/Display.java
@@ -3,7 +3,9 @@ package app;
 import java.applet.Applet;
 import java.awt.Graphics;
 
-public class Display extends Applet {
+public class Display extends Applet implements Runnable {
+
+	public Thread thread = new Thread(this);
 
 	public static int width = 640;
 	public static int height = 640;
@@ -14,12 +16,54 @@ public class Display extends Applet {
 		super.init();
 	}
 
-	
+	@Override
+	public void start() {
+		thread.start();
+		super.start();
+	}
+
+	@Override
+	public void stop() {
+		thread.stop();
+		super.stop();
+	}
+
 	Graphics g;
+
 	@Override
 	public void paint(Graphics g) {
 		add(new Screen(g));
+
+		try {
+			thread.sleep(2000);
+			Screen screen = new Screen(g);
+			screen.update(g);
+
+			if (thread.interrupted()) {
+				thread.destroy();
+			}
+
+			System.gc();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		repaint(7000);
+
 		super.paint(g);
+	}
+
+	@Override
+	public void run() {
+		// try {
+		// thread.sleep(2000);
+		// Screen screen = new Screen(g);
+		// screen.update(g);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 	}
 
 }
--- src/app/Screen.java
+++ src/app/Screen.java
@@ -3,8 +3,10 @@
  */
 package app;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.util.Random;
 
 /**
  * @author FinnT730
@@ -12,20 +14,47 @@ import java.awt.Graphics;
  */
 public class Screen extends Component {
 
+	// Thread thread = new Thread(this);
+
 	public Screen(Graphics g) {
 		paint(g);
+
 	}
 
 	@Override
 	public void paint(Graphics g) {
+
+		g.clearRect(0, 0, Display.width, Display.height);
+
 		Display dis = new Display();
 		for (int i = 0; i < dis.width; i += 10) {
 			for (int o = 0; o < dis.height; o += 10) {
 				g.drawLine(i, o, i + 10, o);
 				g.drawLine(i, o, i, o + 10);
+
+				g.setColor(new Color(new Random().nextInt(255), new Random().nextInt(255), new Random().nextInt(255)));
+				g.fillRect(i, o, 10, 10);
+
+				g.setColor(Color.black);
+
+				int number = new Random().nextInt(9);
+				g.drawString("" + number, i, o);
+
 			}
 		}
 		super.paint(g);
+	}
+	
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.Component#update(java.awt.Graphics)
+	 */
+	@Override
+	public void update(Graphics g) {
+		g.clearRect(0, 0, Display.width, Display.height);
+		super.update(g);
 	}
 
 }
